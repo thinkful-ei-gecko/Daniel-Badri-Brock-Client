00a40d11c34f5f996371856084a15850ca026135
Badri-narayan <badritulsiram@gmail.com>
2019-11-15 14:50:00 -0800

    post user

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -13,12 +13,13 @@ const AuthApiService = {
     );
   },
   postUser(user) {
+    console.log(user)
     return fetch(`${config.API_ENDPOINT}/users`, {
       method: "POST",
       headers: {
         "content-type": "application/json"
       },
-      body: JSON.stringify(user)
+      body: JSON.stringify({user})
     }).then(res =>
       !res.ok ? res.json().then(e => Promise.reject(e)) : res.json()
     );
